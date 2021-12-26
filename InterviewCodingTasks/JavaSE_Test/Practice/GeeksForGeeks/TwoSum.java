ff4514b8d92726b68ba97ee4c62fa199cbfc0107
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2021-12-25 23:23:00 -0500

    anagram very short version with  comment

---
+++ InterviewCodingTasks/JavaSE_Test/Practice/GeeksForGeeks/TwoSum.java
@@ -0,0 +1,24 @@
+package JavaSE_Test.Practice.GeeksForGeeks;
+
+/**
+ * Problem Statement:
+ * Given an array of integers, find two numbers such that they add up to
+ * a specific target number.
+ * The method twoSum(int[] input, target)should return indices of the
+ * two numbers such that they add up to the target, where index1 must be
+ * less than index2. Please note that your returned answers
+ * (both index1 and index2) are not zero-based.
+ *
+ * Note: You may assume that each input would have exactly one solution.
+ *
+ * Example
+ * [12,17,21,25], 29 -> [1,2]
+ */
+public class TwoSum {
+    int target;
+    public static int[] twoSum(int[] input, int target){
+        int[] indexArr=new int[2];
+        return indexArr;
+    }
+
+}
