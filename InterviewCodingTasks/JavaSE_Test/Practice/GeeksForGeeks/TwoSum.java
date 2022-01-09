3be7fd7a5525ebc56c9da5c5dea5e74b6c15aebe
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2022-01-08 21:43:48 -0500

    checking LinkEdIn question- answer at https://technorj.com/linkedin-java-skill-assessment-answers-2021/. Some of their answers are wrong.

--- InterviewCodingTasks/JavaSE_Test/Practice/GeeksForGeeks/TwoSum.java
+++ InterviewCodingTasks/JavaSE_Test/Practice/GeeksForGeeks/TwoSum.java
@@ -1,5 +1,7 @@
 package JavaSE_Test.Practice.GeeksForGeeks;
 
+import java.util.Arrays;
+
 /**
  * Problem Statement:
  * Given an array of integers, find two numbers such that they add up to
@@ -16,9 +18,28 @@ package JavaSE_Test.Practice.GeeksForGeeks;
  */
 public class TwoSum {
     int target;
-    public static int[] twoSum(int[] input, int target){
+    public static int[] twoSum(int[] numbers, int target){
         int[] indexArr=new int[2];
+        //Arrays.sort(numbers);
+
+        //each numbers would have exactly one solution:
+        for (int i = 0; i < numbers.length-1; i++) {
+            int firstNum=numbers[i];
+            for (int j = i+1; j < numbers.length ; j++) {
+                if(numbers[j]==target-firstNum){
+                    indexArr[0]=i;
+                    indexArr[1]=j;
+                    break;
+                }
+            }
+
+        }
+
         return indexArr;
     }
 
+    public static void main(String[] args) {
+        int[] inputArr={25, 21, 12, 17};
+        System.out.println("twoSum({12,17,21,25}, 29) = " + Arrays.toString(twoSum(inputArr, 29)));
+    }
 }
